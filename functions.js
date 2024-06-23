27f555edbe0bcda96a85971dab63cb4538f58a04
Maarten Postma <maarten.postma1000@gmail.com>
2024-06-23 20:05:47 +0200

    Add results in functions.js

--- functions.js
+++ functions.js
@@ -14,20 +14,43 @@ console.log(greeting);
 // Schrijf een functie genaamd getSchool, die de string "Novi Hogeschool" teruggeeft
 // ---- Verwachte uitkomst bij het aanroepen van de functie: "Novi Hogeschool"
 
+function schoolName() {
+  return "Novi Hogeschool";
+}
 
+console.log(schoolName());
 
 /* Opdracht 2 */
 // Schrijf een functie genaamd compliment, die jou een complimentje geeft!
 // ---- Verwachte uitkomst bij het aanroepen van de functie: "Lekker bezig met die functies, Jan!"
 
+function compliment(name) {
+  return "Lekker bezig met die functies, " + name + "!";
+}
 
+console.log(compliment("Maarten"));
 
 /* Opdracht 3 */
 // Schrijf een functie genaamd getGrades, die een array met de cijfers 8, 8.5, 6 en 7 teruggeeft.
 // ---- Verwachte uitkomst bij het aanroepen van de functie: [8, 8.5, 6, 7]
 
+function getGrades() {
+  const arr = [8, 8.5, 6, 7];
+  return arr;
+}
 
+console.log(getGrades());
 
 /* Opdracht 4 */
 // Schrijf een functie genaamd getDetails, die een object met daarin jouw eigen naam en achternaam teruggeeft.
 // ---- Verwachte uitkomst bij het aanroepen van de functie: { firstName: 'Jan', lastName: 'Janssen' }
+
+function getDetails( firstName, lastName) {
+  const details = {
+    firstName: firstName,
+    lastName: lastName,
+  }
+  return details;
+}
+
+console.log(getDetails("Maarten", "Postma"));
